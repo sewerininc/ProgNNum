fa2241202d06ae0ef8bafa47b88b121283a48a3c
Mikkel Sewerin Daugaard - Tester WSL <tester@tester>
2023-02-08 12:55:05 +0100

    made exercise 2

---
+++ exercises/2-csharp/main.cs
@@ -0,0 +1,26 @@
+using System;
+using static System.Console;
+using static System.Math;
+using System.Collections.Generic;
+public static class main{
+	public static void Main(){
+		math_funcs.print();	
+		Write("hello in main\n");
+		math_funcs.to_print="hello from main from math\n";
+		math_funcs.print();
+		WriteLine($"sqrt2 = {Sqrt(2)}");
+		WriteLine($"2^(1/5) = {Pow(2, 1/5)}");
+		WriteLine($"e^pi = {Pow(E, PI)}");
+		WriteLine($"pi^e = {Pow(PI, E)}");
+		WriteLine($"testing \n sqrt2^2 = {Sqrt(2)*Sqrt(2)}");
+		WriteLine($"(e^pi)/(e^pi) = {Pow(E, PI)/Pow(E, PI)}");
+		double svar = math_funcs.gamma(1);
+		WriteLine($"gamma(1) = {svar}");
+		WriteLine($"gamme(1) hurtig = {math_funcs.gamma(1)}");
+		List<int> numbers = new List<int>() {1, 2, 3, 10};
+		foreach (var i in numbers) {
+			WriteLine($"lngamme({i}) = {math_funcs.lngamma(i)}");
+			WriteLine($"gamme({i}) = {math_funcs.gamma(i)}");
+		}
+	}
+}
