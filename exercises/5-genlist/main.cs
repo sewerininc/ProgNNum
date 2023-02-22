3b3b03eb5c8d7a35b7cc9e88d9f42cf52bc91c0c
Mikkel Sewerin Daugaard - Tester WSL <tester@tester>
2023-02-22 13:17:10 +0100

    6 done and 5 working progress

--- exercises/5-genlist/main.cs
+++ exercises/5-genlist/main.cs
@@ -4,10 +4,10 @@ using static System.Console;
 
 public class genlist<T>{
 	public T[] data;
-	public int size => data.Length; // property
-	public T this[int i] => data[i]; // indexer
+	public int size => data.Length;
+	public T this[int i] => data[i]; 
 	public genlist(){ data = new T[0]; }
-	public void add(T item){ /* add item to the list */
+	public void add(T item){
 		T[] newdata = new T[size+1];
 		System.Array.Copy(data,newdata,size);
 		newdata[size]=item;
---
+++ exercises/6-mulitproc/main.cs
@@ -0,0 +1,57 @@
+using System;
+using static System.Math;
+using static System.Console;
+using System.Threading;
+using System.Threading.Tasks;
+
+static public class main{
+	
+	public class datas{public int a,s; public double partsum;}
+	public static void harm_sum(object obj){
+		var local = (datas)obj;
+		local.partsum = 0;
+		for( int i = local.a; i < local.s;i++)local.partsum +=1.0/i;
+	}		
+	
+	
+	public static void Main(string[] args){
+		int threads = 1;
+		int to_sum = (int)2e8;
+		foreach(var arg in args)
+		{
+			var words = arg.Split(':');
+			if (words[0] == "-threads") threads = int.Parse(words[1]);
+			else if (words[0] == "-to_sum") to_sum = (int)float.Parse(words[1]);
+			else WriteLine($"Nothing to be done for input {words}.\nSyntaxes -threads:number or -to_sum:number");
+		}	
+		WriteLine($"to_sum = {to_sum}, threads = {threads}");
+		
+		datas[] data = new datas[threads];
+		for(int i=0;i<threads;i++)
+		{
+			data[i] = new datas();
+			data[i].a = 1 + (to_sum * i) / threads;
+			data[i].s = 1 + (to_sum * (i + 1)) / threads;
+			WriteLine($"i = {i}, a = {data[i].a}, s = {data[i].s}");
+		}
+		
+		var threads_cpu = new Thread[threads];
+	       	for(int i=0;i<threads;i++)
+		{
+			threads_cpu[i] = new Thread(harm_sum);
+			threads_cpu[i].Start(data[i]);
+		}
+		
+		for(int i=0; i<threads;i++)
+		{
+			threads_cpu[i].Join();
+		}
+
+		double tot_sum = 0;
+		for(int i=0; i<threads;i++) tot_sum += data[i].partsum;
+		WriteLine($"tot_sum = {tot_sum}");
+
+
+	}
+
+}
