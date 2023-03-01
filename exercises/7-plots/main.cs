03f67674c068970682822713a82987079b458c45
Mikkel Sewerin Daugaard - Tester WSL <tester@tester>
2023-03-01 11:49:11 +0100

    plots made

--- exercises/7-plots/main.cs
+++ exercises/7-plots/main.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 static public class main{
 	
-	public static void Main(){
-		for(double x=-5+1.0/128;x<=5;x+=1.0/5){
-		       WriteLine($"{x} {math_funcs.gamma(x)}");
-		}	       
-
+	public static void Main(string[] args){
+		foreach(var arg in args){
+			if(arg == "gamma"){
+				for(double x=-5+1.0/128;x<=5;x+=1.0/64){
+		       			WriteLine($"{x} {math_funcs.gamma(x)}");
+				}	       
+			}
+			else if(arg == "lngamma"){
+				for(double x=-5+1.0/128;x<=5;x+=1.0/64){
+		       			WriteLine($"{x} {math_funcs.lngamma(x)}");
+				}
+			}
+			else if(arg == "error"){
+				for(double x=-5+1.0/128;x<=5;x+=1.0/64){
+		       			WriteLine($"{x} {math_funcs.error(x)}");       
+				}
+			}
+			else{
+				WriteLine("Wrong input - the 3 options are (gamma, error, lngamma)");
+			}
+		}
 	}
 
 }
--- exercises/7-plots/math_funcs.cs
+++ exercises/7-plots/math_funcs.cs
@@ -22,4 +22,13 @@ public class math_funcs{
         return lgamma;
 	}
 
+	public static double error(double x){
+        /// Approx from dimitri from Wikipedia 
+	if(x<0) return -error(-x);
+	double[] a={0.254829592,-0.284496736,1.421413741,-1.453152027,1.061405429};
+	double t=1/(1+0.3275911*x);
+	double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));/* the right thing */
+	return 1-sum*Exp(-x*x);
+	}
+
 }
