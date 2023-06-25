b721865f0cc5ef7683cf2e8e1d4ef7f7d3690600
Mikkel Sewerin Daugaard - Tester WSL <tester@tester>
2023-06-25 15:53:23 +0200

    finshed b task in splines

--- homeworks/splines/b_task/main.cs
+++ homeworks/splines/b_task/main.cs
@@ -7,114 +7,117 @@ using System.IO;
 using System.Text;
 public static class main{
 
-	public static int binsearch(double[] x, double z){/* locates the interval for z by bisection */ 
-		if(!(x[0]<=z && z<=x[x.Length-1])) throw new Exception("binsearch: bad z");
-		int i=0, j=x.Length-1;
-		while(j-i>1){
-			int mid=(i+j)/2;
-			if(z>x[mid]) i=mid; else j=mid;
+	public static double get_p(vector xs, vector ys, int i){
+		return (ys[i+1]-ys[i])/(xs[i+1]-xs[i]);
+	}
+
+	public static double get_c_forward(vector xs, vector ys, vector cs, int i){/* We get c_i+1 */
+		return 1.0/(xs[i+2] - xs[i+1])*(get_p(xs, ys, i+1) - get_p(xs, ys, i) - cs[i]*(xs[i+1]-xs[i]));
+	}
+
+	public static double get_c_backward(vector xs, vector ys, vector cs, int i){/* We get c_i */
+		return 1.0/(xs[i+1] - xs[i])*(get_p(xs, ys, i+1) - get_p(xs, ys, i) - cs[i+1]*(xs[i+2]-xs[i+1]));
+	}
+	
+	public static double get_b(vector xs, vector ys, vector cs, int i){
+		return get_p(xs, ys, i) - cs[i]*(xs[i+1]-xs[i]);
+	}
+	
+	public static double get_s(double x, vector xs, vector ys, vector bs, vector cs){
+		int i = 0;
+		int j = xs.size - 1;
+		
+		while(j-i > 1){
+			int m = (i+j)/2;
+			if(x > xs[m]) i = m;
+			else j = m;
 		}
-		return i;
+	
+		return ys[i] + bs[i]*(x-xs[i]) + cs[i]*Pow(x-xs[i], 2);
+	}
+	
+	public static (vector, vector) get_bs_cs(vector xs, vector ys){
+		int n = xs.size;
+		vector cs_f = new vector(n);
+		vector cs_b = new vector(n);
+		vector bs = new vector(n);
+	
+		for(int i=0; i<n-2; i++) cs_f[i+1] = get_c_forward(xs, ys, cs_f, i);
+		cs_b[n-2] = 1/2*cs_f[n-2];
+		for(int i=n-3; i>=1; i--) cs_b[i] = get_c_backward(xs, ys, cs_b, i);
+		vector cs = (cs_b+cs_f)/2;
+		for(int i=0; i<n-1; i++) bs[i] = get_b(xs, ys, cs, i);
+		return (bs, cs);
 	}
 
-	public static double linterp(double[] x, double[] y, double z){
-		int i=binsearch(x,z);
-		double dx=x[i+1]-x[i]; if(!(dx>0)) throw new Exception("uups...");
-		double dy=y[i+1]-y[i];
-		return y[i]+dy/dx*(z-x[i]);
-	}
 
-	public static double linterpInteg(double[] x, double[] y, double z){/*
-		calculates integral of the linear spline function
-		x needs to be sorted such lowest value in x[0]
-		*/
-		double yz = linterp(x, y, z);
-		double int_val = 0;
-		for(int i=0;i<x.Length-1;i++){
-			if(x[i+1] >= z){
-				double b = (yz-y[i])/(z-x[i]);
-				double a = y[i]-b*x[i];	
-				int_val += -1/2.0*(x[i]-z)*(2*a+b*(x[i]+z));
-				break;
-			}
-			else{	
-				double b = (y[i+1]-y[i])/(x[i+1]-x[i]);
-				double a = y[i]-b*x[i];	
-				int_val += -1/2.0*(x[i]-x[i+1])*(2*a+b*(x[i]+x[i+1]));
-			}
-		}
-		return int_val;
-	}	
-	typedef struct { int n ; double ∗x , ∗y , ∗b , ∗ c ; } q s p l i n e ;
-s−>b = (double ∗) m all oc ( ( n−1)∗ s i z e o f (double ) ) ; // b i
-s−>c = (double ∗) m all oc ( ( n−1)∗ s i z e o f (double ) ) ; // c i
-s−>x = (double ∗) m all oc ( n∗ s i z e o f (double ) ) ; // x i
-s−>y = (double ∗) m all oc ( n∗ s i z e o f (double ) ) ; // y i
-s−>n = n ; fo r ( int i =0; i<n ; i ++){s−>x [ i ]=x [ i ] ; s−>y [ i ]=y [ i ] ; }
-int i ; double p [ n−1] , h [ n −1]; //VLA from C99
-fo r ( i =0; i<n−1; i ++){h [ i ]=x [ i +1]−x [ i ] ; p [ i ]=( y [ i +1]−y [ i ] ) / h [ i ] ; }
-s−>c [ 0 ] = 0 ; // r e c u r s i o n up :
-fo r ( i =0; i<n−2; i++)s−>c [ i +1]=(p [ i +1]−p [ i ]−s−>c [ i ] ∗ h [ i ] ) / h [ i + 1];
-s−>c [ n−2]/=2; // r e c u r s i o n down :
-fo r ( i=n−3; i >=0;i −−)s−>c [ i ]=( p [ i +1]−p [ i ]−s−>c [ i +1]∗h [ i +1])/ h [ i ] ;
-fo r ( i =0; i<n−1; i++)s−>b [ i ]=p [ i ]−s−>c [ i ] ∗ h [ i ] ;
-return s ; }
-double q s p l i n e e v a l ( q s p l i n e ∗s , double z ){ // e v a l u a t e s s ( z )
-a s s e r t ( z>=s−>x [ 0 ] && z<=s−>x [ s−>n − 1] );
-int i =0, j=s−>n−1; // b i n a r y se a rc h :
-while ( j−i >1){ int m=( i+j ) / 2 ; i f ( z>s−>x [m] ) i=m; e l s e j=m; }
-double h=z−s−>x [ i ] ;
-return s−>y [ i ]+h∗( s−>b [ i ]+h∗s−>c [ i ] ) ; }
-
-
-	public static double[] polynomion(double[] xs, double a, double b, double c){
-		double[] ys = new double[xs.Length];
-		for(int i=0;i<xs.Length;i++) ys[i] = a + xs[i]*b + Pow(xs[i],2)*c;
-		return ys;
-	}
-
-	public static void Main(string[] args){
-		double[] xs = new double[] {0, 3, 5, 7, 9};
-		double[] ys = new double[] {1, 1, 1, 1, 1};
-		int n = 21;
-		double[] rndxs = new double[n];
-		for(int i=0; i<n;i++) rndxs[i]=i-10.0;
-		double[] rndys = new double[n];
-		var rnd = new System.Random();
-		for(int i=0; i<n;i++) rndys[i]=rnd.Next(21)-10;
-		foreach(var arg in args){
-			if(arg == "cterpdata"){
-				for(double i=xs.Min();i<=xs.Max();i+=1.0/20) WriteLine($"{i} {linterp(xs, ys, i)}");		
-			}
-			if(arg == "rndterpdata"){
-				var rnd_data = File.ReadAllText("rnddata.data").Split("\n");
-				for(int i =0; i<rnd_data.Length-1; i++){
-					var xys = rnd_data[i].Split(' ');
-				       	rndxs[i] = Double.Parse(xys[0]);
-					rndys[i] = Double.Parse(xys[1]);
-				}
-				for(double i=rndxs.Min();i<=rndxs.Max();i+=1.0/20) WriteLine($"{i} {linterp(rndxs, rndys, i)}");
-			}	
-			if(arg == "rnddata"){
-				for(int i=0; i<rndxs.Length;i++) WriteLine($"{rndxs[i]} {rndys[i]}");
-			}
-			if(arg == "cdata"){
-				for(int i=0; i<xs.Length;i++) WriteLine($"{xs[i]} {ys[i]}");
-			}
-			if(arg == "test"){
-				var rnd_data = File.ReadAllText("rnddata.data").Split("\n");
-				for(int i =0; i<rnd_data.Length-1; i++){
-					var xys = rnd_data[i].Split(' ');
-				       	rndxs[i] = Double.Parse(xys[0]);
-					rndys[i] = Double.Parse(xys[1]);
-				}	
-				WriteLine($"Integral of constant (const_plot.svg) function y=1 from 0 to 2.1 = {linterpInteg(xs, ys, 2.1)}");
-				WriteLine($"Integral of random points (rand_plot.svg) from -10 to 5.3 = {linterpInteg(rndxs, rndys, 5.3)}");
-			}
+	public static void Main(){
+		int n = 5;
+		vector xs = new vector(n);
+		vector ys = new vector(n);
+		
+		for(int i=0; i<n; i++){
+			xs[i] = i+1;
+			ys[i] = 1;
 		}
 		
+		(vector bs, vector cs) = get_bs_cs(xs, ys);
+		
+		for(int i=n-2; i>1; i--) WriteLine(i);
+		
+		WriteLine("I will show these 3 solutions found and the analytical solution\n");
+		
+		xs.print("xs = ");
+		ys.print("ys = ");
+		WriteLine("Analytic solution: b[i]=c[i]=0");
+		WriteLine("i, b[i], c[i]");
+		for(int i=1; i<n-1; i++) WriteLine($"{i}, {bs[i]}, {cs[i]}");
+		
+		WriteLine("\n");
+		
+		for(int i=0; i<n; i++){
+			xs[i] = i+1;
+			ys[i] = i+1;
+		}
+		xs.print("xs = ");
+		ys.print("ys = ");
+		(bs, cs) = get_bs_cs(xs, ys);
+		WriteLine("Analytic solution: b[i]=1, c[i]=0");
+		WriteLine("i, b[i], c[i]");
+		for(int i=1; i<n-1; i++) WriteLine($"{i}, {bs[i]}, {cs[i]}");
+		
+		WriteLine("\n");
+		
+		for(int i=0; i<n; i++){
+			xs[i] = i+1;
+			ys[i] = Pow(i+1, 2);
+		}
+		xs.print("xs = ");
+		ys.print("ys = ");
+		(bs, cs) = get_bs_cs(xs, ys);
+		WriteLine("Analytic solution: b[i]=2*x[i], c[i]=1");
+		WriteLine("i, b[i], c[i], x[i]");
+		for(int i=1; i<n-1; i++) WriteLine($"{i}, {bs[i]}, {cs[i]}, {xs[i]}");
 
 
 			
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
